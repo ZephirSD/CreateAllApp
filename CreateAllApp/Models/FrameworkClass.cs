e025adcf7055242f87dda3dc5f9ac33bad28c4e3
ZephirSD <siegfrieddallery@gmail.com>
2023-10-14 20:23:00 +0400

    Création de la classe `FrameworkClass`

---
+++ CreateAllApp/Models/FrameworkClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAllApp.Models
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    public class FrameworkClass
+    {
+        public string Nom {  get; set; }
+        public Uri Logo { get; set; }
+        public string Commande { get; set; }
+
+        public FrameworkClass(string nom,  Uri logo, string commande)
+        {
+            Nom = nom;
+            Logo = logo;
+            Commande = commande;
+        }
+    }
+}
