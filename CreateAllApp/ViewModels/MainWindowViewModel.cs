8a12153fe4dcbc806866d328d8718b3b049ff97f
ZephirSD <siegfrieddallery@gmail.com>
2023-10-24 17:43:09 +0400

    Ajout des Frameworks avec la classe `FrameworkClass`

--- CreateAllApp/ViewModels/MainWindowViewModel.cs
+++ CreateAllApp/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CreateAllApp.Models;
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace CreateAllApp.ViewModels
 {
@@ -9,14 +10,15 @@ namespace CreateAllApp.ViewModels
         public ObservableCollection<FrameworkClass> Frameworks { get; set; } = new ObservableCollection<FrameworkClass>();
         public MainWindowViewModel()
         {
-            Frameworks.Add(new FrameworkClass("ReactJS", new Uri("../Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "npx create-react-app"));
-            Frameworks.Add(new FrameworkClass("NextJS", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "npx create-next-app@latest"));
-            Frameworks.Add(new FrameworkClass("NuxtJS", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "npx nuxi@latest init"));
-            Frameworks.Add(new FrameworkClass("Flutter", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "flutter create"));
-            Frameworks.Add(new FrameworkClass("Symfony", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "symfony new"));
-            Frameworks.Add(new FrameworkClass("Laravel", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "composer create-project laravel/laravel"));
-            Frameworks.Add(new FrameworkClass("VueJS", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "npm create vue@latest"));
-            Frameworks.Add(new FrameworkClass("Svelte", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "npm create svelte@latest"));
+            Frameworks.Add(new FrameworkClass("ReactJS", new Uri("../Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "npx create-react-app {0}"));
+            Frameworks.Add(new FrameworkClass("NextJS", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "npx create-next-app@latest {0}"));
+            Frameworks.Add(new FrameworkClass("NuxtJS", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "npx nuxt@latest init {0}"));
+            Frameworks.Add(new FrameworkClass("Flutter", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "flutter create {0}"));
+            Frameworks.Add(new FrameworkClass("Symfony", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "symfony new {0} --version=\"6.3.*\" --webapp"));
+            Frameworks.Add(new FrameworkClass("Laravel", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "composer create-project laravel/laravel {0}"));
+            Frameworks.Add(new FrameworkClass("VueJS", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "npm create vue@latest {0}"));
+            Frameworks.Add(new FrameworkClass("Svelte & Vite", new Uri("/Assets/avalonia-logo.ico", UriKind.RelativeOrAbsolute), "npm create svelte@latest {0}"));
         }
+
     }
 }
